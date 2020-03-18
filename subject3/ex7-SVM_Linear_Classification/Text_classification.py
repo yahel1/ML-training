90463f5781727faa662ed5182c9a0fcdc8d29b30
yahel1 <yyahell1@gmail.com>
2020-03-18 17:22:52 +0200

    done

--- subject3/ex7-SVM_Linear_Classification/Text_classification.py
+++ subject3/ex7-SVM_Linear_Classification/Text_classification.py
@@ -1,21 +1,12 @@
-import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.datasets import load_svmlight_file
-from scipy.sparse import csr_matrix
 from sklearn import svm
 
-
 if __name__ == '__main__':
-    train_features, train_labels = load_svmlight_file('twofeature.txt')
-    x0, x1 = csr_matrix.toarray(train_features).T
-    colors = ['red' if label == 1 else 'green' for label in train_labels]
-    xx, yy = make_meshgrid(x0, x1)
-    fig, ax = plt.subplots(1, 2)
-    indC = 0
-    for C in [1, 100]:
-        clf = svm.SVC(kernel='linear', C=C)
-        clf.fit(np.array([x0, x1]).T, train_labels)
-        plot_contours(ax[indC], clf, xx, yy)
-        ax[indC].scatter(x0, x1, c=train_labels, cmap=plt.cm.coolwarm, edgecolors='k')
-        ax[indC].set_title(f'C={C}')
-        indC += 1
+    test_features, test_labels = load_svmlight_file('email_test.txt')
+    for set_size in ['50', '100', '400', 'all']:
+        file_name = 'email_train-'+set_size+'.txt'
+        train_features, train_labels = load_svmlight_file(file_name, n_features=test_features.shape[1])
+        clf = svm.SVC(kernel='linear', C=1)
+        clf.fit(train_features, train_labels)
+        accuracy = 100*clf.score(test_features, test_labels)
+        print(set_size + f' documents: Accuracy = {accuracy:.4}%')
